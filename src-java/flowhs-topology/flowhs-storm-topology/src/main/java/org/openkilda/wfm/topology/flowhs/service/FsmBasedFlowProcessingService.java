e41911e1ab54697f2d08b128f2ba66aaf8c1de8c
Sergii Iakovenko <sergii.iakovenko@gmail.com>
2021-09-23 07:57:32 +0300

    Implement YFlow create feature.

---
+++ src-java/flowhs-topology/flowhs-storm-topology/src/main/java/org/openkilda/wfm/topology/flowhs/service/FsmBasedFlowProcessingService.java
@@ -0,0 +1,99 @@
+/* Copyright 2021 Telstra Open Source
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.openkilda.wfm.topology.flowhs.service;
+
+import org.openkilda.persistence.PersistenceManager;
+import org.openkilda.persistence.repositories.FlowRepository;
+import org.openkilda.persistence.repositories.RepositoryFactory;
+import org.openkilda.wfm.share.utils.FsmExecutor;
+import org.openkilda.wfm.topology.flowhs.fsm.common.FlowProcessingFsm;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+public abstract class FsmBasedFlowProcessingService<T extends FlowProcessingFsm<T, ?, E, C, R>, E, C,
+        R extends FlowGenericCarrier> {
+    private final Map<String, T> fsmByKey = new HashMap<>();
+    private final Map<String, String> keyByFlowId = new HashMap<>();
+    protected final FsmExecutor<T, ?, E, C> fsmExecutor;
+
+    protected final R carrier;
+    protected final FlowRepository flowRepository;
+
+    @Getter(AccessLevel.PROTECTED)
+    private boolean active;
+
+    public FsmBasedFlowProcessingService(FsmExecutor<T, ?, E, C> fsmExecutor,
+                                         R carrier, PersistenceManager persistenceManager) {
+        this.fsmExecutor = fsmExecutor;
+        this.carrier = carrier;
+        RepositoryFactory repositoryFactory = persistenceManager.getRepositoryFactory();
+        flowRepository = repositoryFactory.createFlowRepository();
+    }
+
+    protected void registerFsm(String key, T fsm) {
+        fsmByKey.put(key, fsm);
+        keyByFlowId.put(fsm.getFlowId(), key);
+    }
+
+    protected boolean hasRegisteredFsmWithKeyOrFlowId(String key, String flowId) {
+        return fsmByKey.containsKey(key) || keyByFlowId.containsKey(flowId);
+    }
+
+    protected boolean hasAnyRegisteredFsm() {
+        return !fsmByKey.isEmpty();
+    }
+
+    protected Optional<T> getFsmByKey(String key) {
+        return Optional.ofNullable(fsmByKey.get(key));
+    }
+
+    protected Optional<T> getFsmByFlowId(String flowId) {
+        return getKeyByFlowId(flowId).map(fsmByKey::get);
+    }
+
+    protected Optional<String> getKeyByFlowId(String flowId) {
+        return Optional.ofNullable(keyByFlowId.get(flowId));
+    }
+
+    protected void unregisterFsm(String key) {
+        T removed = fsmByKey.remove(key);
+        if (removed != null) {
+            keyByFlowId.remove(removed.getFlowId());
+        }
+    }
+
+    /**
+     * Handles deactivate command.
+     */
+    public boolean deactivate() {
+        active = false;
+        return fsmByKey.isEmpty();
+    }
+
+    /**
+     * Handles activate command.
+     */
+    public void activate() {
+        active = true;
+    }
+}
